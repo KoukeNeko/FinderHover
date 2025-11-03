a26484afa68e23df8536024ae54d173182dd5023
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-11-03 13:03:35 +0800

    Initial Commit

---
+++ FinderHover/FinderHoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderHoverApp.swift
+//  FinderHover
+//
+//  Created by 陳德生 on 2025/11/3.
+//
+
+import SwiftUI
+
+@main
+struct FinderHoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
